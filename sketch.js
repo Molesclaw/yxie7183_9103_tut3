5de306a579401d69504c9766d1f7a201cb9222a9
Yusong Xie <yxie7183@uni.sydney.edu.au>
2024-05-19 03:54:54 +1000

    Add windowResized function
    Add windowResized() function  Set  and update properties to fit this function. It finally works!

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
-//set properties for the Mondrian painting
+//Set properties for the Mondrian painting
 let rectSize = 40;
 
+//Make an object to hold the properties of the Mondrian design
+let mondrian = {aspect: 0, width: 600, height: 600, xOffset: 0, yOffset: 0};
+//Set width equal to height, because I want to make a square design
+
+//A variable for the canvas aspect ratio
+let canvasAspectRatio = 0;
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   background(255, 250, 240); //Floralwhite
   noLoop(); //make the draw function only run once
+  calculateMondrian();
 }
 
 function draw() {
@@ -17,11 +25,11 @@ function drawGrid(){
   //rectangle layout
   //This divides the whole canvas into grids, so that the specific grid
   //can be filled with color.
-  for (let y = 0; y < height; y += rectSize){
-    for (let x = 0; x < width; x += rectSize){
+  for (let y = 0; y < mondrian.height; y += rectSize){
+    for (let x = 0; x < mondrian.width; x += rectSize){
       fill(255, 250, 240);
       noStroke(); //remove the outline of the grids
-      square(x, y, rectSize);
+      square(x + mondrian.xOffset, y + mondrian.yOffset, rectSize);
     }
   }
 }
@@ -30,8 +38,8 @@ function drawGrid(){
 function drawRectangle(){
   for (let i = 0; i < 25; i ++){
     //floor() is used to round the number down to the nearest whole number
-    let x = floor(random(width/rectSize)) * rectSize;
-    let y = floor(random(height/rectSize)) * rectSize;
+    let x = floor(random(mondrian.width/rectSize)) * rectSize;
+    let y = floor(random(mondrian.height/rectSize)) * rectSize;
     let w = random(1, 1.5) * rectSize;
     let h = random(1, 1.5) * rectSize;
 
@@ -42,7 +50,7 @@ function drawRectangle(){
     fill(randomColor);
     stroke(255, 250, 240);
     strokeWeight(1);
-    rect(x, y, w, h);
+    rect(x + mondrian.xOffset, y + mondrian.yOffset, w, h);
   }
 }
 
@@ -53,12 +61,12 @@ function drawLine(){
 
   //Draw Horizontal lines
   for (let i = 0; i < random(8,10); i ++){
-    let y = floor(random(height/rectSize)) * rectSize;
+    let y = floor(random(mondrian.height/rectSize)) * rectSize;
     let h = rectSize/2;
 
     fill(238,216,34);
     stroke(255, 250, 240);
-    rect(0, y, width, h);
+    rect(mondrian.xOffset, y + mondrian.yOffset, mondrian.width, h);
     
     //store the y and h values in the array, so the cross points can be 
     //drawn later
@@ -66,7 +74,7 @@ function drawLine(){
 
     //Add random colored squares along the horizontal line to mimic 
     //Mondrian painting
-    for (let i = rectSize; i < width; i += rectSize){
+    for (let i = rectSize; i < mondrian.width; i += rectSize){
       if(random() > 0.5){ //Randomly decide to place a colored square
         let randomColor = random([color(238,216,34), //yellow
                                   color(173,57,42), //red
@@ -74,25 +82,25 @@ function drawLine(){
                                   color(200, 200, 200)]); //grey
         fill(randomColor);
         noStroke();
-        square(i, y, rectSize/2);
+        square(i + mondrian.xOffset, y + mondrian.yOffset, rectSize/2);
       }
     }
   }
 
   //Draw Vertical lines
   for (let i = 0; i < random(8,10); i ++){
-    let x = floor(random(width/rectSize)) * rectSize;
+    let x = floor(random(mondrian.width/rectSize)) * rectSize;
     let w =  rectSize/2;
 
     fill(238,216,34);
     stroke(255, 250, 240);
-    rect(x, 0, w, height);
+    rect(x + mondrian.xOffset, mondrian.yOffset, w, mondrian.height);
     
     //store the x and w values in the array
     verticalLines.push({x: x, w: w});
   
     //Add random colored squares along the vertical line
-    for (let i = rectSize; i < height; i += rectSize){
+    for (let i = rectSize; i < mondrian.height; i += rectSize){
       if(random() > 0.5){
         let randomColor = random([color(238,216,34), //yellow
                                   color(173,57,42), //red
@@ -100,7 +108,7 @@ function drawLine(){
                                   color(200, 200, 200)]); //grey
         fill(randomColor);
         noStroke();
-        square(x, i, rectSize/2);
+        square(x + mondrian.xOffset, i + mondrian.yOffset, rectSize/2);
       }
     }
   }
@@ -110,14 +118,44 @@ function drawLine(){
   for (let horizontal of horizontalLines){ 
     for (let vertical of verticalLines){
       //
-      if(vertical.x < width && horizontal.y < height){ 
+      if(vertical.x < mondrian.width && horizontal.y < mondrian.height){ 
         let randomColor = random([color(173,57,42),   //red
                                   color(67,103,187),    //blue
                                   color(200, 200, 200)]);  //grey
       
         fill(randomColor);
-        square(vertical.x, horizontal.y, rectSize/2);
+        square(vertical.x + mondrian.xOffset, 
+               horizontal.y + mondrian.yOffset, rectSize/2);
       }
     }
+  }
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+  background(255, 250, 240);
+  calculateMondrian(); 
+  draw();
+}
+
+function calculateMondrian(){
+  canvasAspectRatio = width/height;
+  mondrian.aspect = 1; //Square aspect ratio
+  
+  if(1 > canvasAspectRatio){
+    mondrian.width = width;
+    mondrian.height = width / mondrian.aspect;
+    mondrian.yOffset = (height - mondrian.height) / 2;
+    mondrian.xOffset = 0;
+  } else if (1 < canvasAspectRatio){
+    mondrian.width = height * mondrian.aspect;
+    mondrian.height = height;
+    mondrian.xOffset = (width - mondrian.width) / 2;
+    mondrian.yOffset = 0;
+  } else if (1 == canvasAspectRatio){
+    mondrian.width = width;
+    mondrian.height = height;
+    mondrian.xOffset = 0;
+    mondrian.yOffset = 0;
   }
 }
